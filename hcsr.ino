f4ab72c47694028b5776bfa8f4d1032e3860891f
Muhammad Dhafi Alfaridzi <93527916+alfaridhafi@users.noreply.github.com>
2022-12-03 10:47:13 +0700

    Delete hcsr.ino

--- hcsr.ino
+++
@@ -1,9 +0,0 @@
-void hcsr() {
-  digitalWrite(trigPin, LOW); delayMicroseconds(2);
-  digitalWrite(trigPin, HIGH); delayMicroseconds(10);
-  digitalWrite(trigPin, LOW);
-  duration = pulseIn(echoPin, HIGH);
-  distance = duration / 58.2;
-  Serial.println(distance);
-  delay(50);
-}
